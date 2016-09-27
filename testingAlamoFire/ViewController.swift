a7208251bda0affe2fc0a27cbb1dda1c4f7664d9
ThuReinTun <thureintun.pm@gmail.com>
2016-09-27 17:20:33 +0630

    Initial Commit

---
+++ testingAlamoFire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testingAlamoFire
+//
+//  Created by ThuRein Tun on 9/27/16.
+//  Copyright © 2016 ThuRein Tun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
